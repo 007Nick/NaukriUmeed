a1d229a68a59f3ffa62dc58651a0efb52bd4970a
rajanarora26 <rajan260919@gmail.com>
2021-03-07 14:20:31 +0530

    changes done on the application properties

--- Backend/naukri_umeed/src/main/java/com/example/naukri_umeed/NaukriUmeedApplication.java
+++ Backend/naukri_umeed/src/main/java/com/example/naukri_umeed/NaukriUmeedApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NaukriUmeedApplication {
+public class NaukriUmeedApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(NaukriUmeedApplication.class, args);
